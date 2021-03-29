cd291e6b57b056af2c9f37510472ea9b7ec6e283
Evgen Shmat <evgeniy.shmat@gmail.com>
2021-03-29 22:47:43 +0300

    feat: make first task

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,28 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+// module.exports = function countCats(backyard) {
+//   let count = 0;
+//   for (let i = 0; i < backyard.length; i++) {
+//     for (let j = 0; j < backyard[i].length; j++){
+//       if (backyard[i][j] == "^^") {
+        
+//         count++;
+//         return (count);
+//       }
+//       else { return 0;}
+//     }
+//   }
+  
+// };
+module.exports = function countCats(backyard) {
+  let count = 0;
+  for (let subArr of backyard) {
+    subArr.forEach((item) => {
+      if (item == "^^") {
+        count++;
+      }
+    });
+    
+  }
+  return count;
 };
