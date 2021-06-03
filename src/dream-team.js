b60c1e7a59d9d938cbe6ea7bc9d16b16ba153124
Evgen Shmat <evgeniy.shmat@gmail.com>
2021-06-03 13:27:38 +0300

    second task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,45 @@
-const CustomError = require("../extensions/custom-error");
+// const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+
+module.exports = function createDreamTeam(members) {
+	let newArr;
+	if (members.constructor === Array) {
+		newArr = members;
+
+		newArr = newArr.map(function (item, index, array) {
+			if (typeof (item) === 'string') {
+				return (item.slice(0, 1).toUpperCase());
+			}
+		});
+
+	} else {
+		return members = 'false';
+	}
+
+
+	return (newArr.sort().join(''));
 };
+
+
+
+
+// let arr = ['Bobby', 'Kevin', 'tere sa', null,1,1,'Betty', 'Lori', 'Joyce', 'Scott', 'Johnny', 'Cheryl', 'Ronald'];
+// function createDreamTeam(members) {
+// 	let newArr;
+// 	if (members.constructor === Array) {
+// 		newArr = members;
+		
+// 		newArr = newArr.map(function (item, index, array) {
+// 			if (typeof (item) === 'string') {
+// 				return(item.slice(0, 1).toUpperCase());
+// 			}
+// 		});
+	
+// 	} else {
+// 		return false;
+// 	}
+	
+	
+// 	console.log(newArr.sort().join(''));
+// }
+// createDreamTeam(arr);
